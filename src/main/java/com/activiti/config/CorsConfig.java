96472c1d6d820a07bf21f5a541c212f3ffc5ce39
cuihang <184136280@qq.com>
2020-03-06 11:09:56 +0800

    cors 携带令牌

--- src/main/java/com/activiti/config/CorsConfig.java
+++ src/main/java/com/activiti/config/CorsConfig.java
@@ -1,5 +1,8 @@
 package com.activiti.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,28 +13,53 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-private CorsConfiguration buildConfig() {
-    CorsConfiguration corsConfiguration = new CorsConfiguration();
-    corsConfiguration.addAllowedOrigin("*");
-    corsConfiguration.addAllowedHeader("*");
-    corsConfiguration.addAllowedMethod("*");
-    corsConfiguration.addExposedHeader("Authorization");
-    return corsConfiguration;
-}
- 
-@Bean
-public CorsFilter corsFilter() {
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", buildConfig());
-    return new CorsFilter(source);
-}
- 
-@Override
-public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowCredentials(true)
-            .allowedMethods("GET", "POST", "DELETE", "PUT")
-            .maxAge(3600);
-}
+	
+//	private CorsConfiguration buildConfig() {
+//	    CorsConfiguration corsConfiguration = new CorsConfiguration();
+//	    corsConfiguration.setAllowCredentials(true);
+//	    corsConfiguration.addAllowedOrigin("*");
+//	    corsConfiguration.addAllowedHeader("*");
+//	    corsConfiguration.addAllowedMethod("*");
+//	    corsConfiguration.addExposedHeader("Authorization");
+//	    List<String> list = new ArrayList<>();
+//        list.add("Origin");
+//        list.add("Date");
+//        corsConfiguration.setExposedHeaders(list);
+//	    return corsConfiguration;
+//	}
+//	 
+//	@Bean
+//	public CorsFilter corsFilter() {
+//	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//	    source.registerCorsConfiguration("/**", buildConfig());
+//	    return new CorsFilter(source);
+//	}
+	
+	@Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true); //允许携带身份令牌
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        List<String> list = new ArrayList<>();
+        list.add("Origin");
+        list.add("Date");
+        corsConfiguration.setExposedHeaders(list);
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+	
+	
+	
+	 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/**")
+	            .allowedOrigins("*")
+	            .allowCredentials(true)
+	            .allowedMethods("GET", "POST", "DELETE", "PUT")
+	            .maxAge(3600);
+	}
 }
